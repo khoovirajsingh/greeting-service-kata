b6763c81c45c8b5a3b2a902a9071761eea962e17
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-09-11 11:37:30 +0100

    Setup

---
+++ src/Application.kt
@@ -0,0 +1,34 @@
+package com.katas
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
