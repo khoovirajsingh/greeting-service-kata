7730def3fc81d1a17a7ad4d7b0fc3c6dfb9c11b4
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-09-11 12:05:32 +0100

    Greeting a User by choosing a random greeting message from a set of messages

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -6,20 +6,34 @@ import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ApplicationTest {
     @Test
-    fun testGreeting() {
+    fun `just a greeting`() {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/greeting").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello my friend!", response.content)
             }
+        }
+    }
 
+    @Test
+    fun `greeting a user by choosing a random greeting message from a set of messages`() {
+        withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/greeting/joe").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello Joe!", response.content)
+                assertTrue(predefinedMessagesFor("Joe").contains(response.content))
             }
         }
+    }
+
+    private fun predefinedMessagesFor(user: String): List<String> {
+        val hello = "Hello $user!"
+        val hey = "Hey $user, nice to see you here!"
+        val welcome = "$user welcome back!"
+        val splendid = "Have a splendid day $user."
+        return listOf(hello, hey, welcome, splendid)
     }
 }
